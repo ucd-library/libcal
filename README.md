fb783bfe5ec382d6e40eab1935e8004f9caf889a
Mark Warren <mjwarren@ucdavis.edu>
2022-08-12 11:14:22 -0700

    add libcal

---
+++ README.md
@@ -0,0 +1,6 @@
+# LibCal
+
+[LibCal](https://reservations.library.ucdavis.edu//) is a [Springshare](https://www.springshare.com/) product used by librarians to create and display subject and course guides. This repository contains files for:
+
+- Initial data migration from WordPress into LibCal
+- JS and CSS for applying the UC Davis digital brand.
---
+++ client/src/index.js
@@ -0,0 +1,17 @@
+// UCD Theme Custom Elements
+import '@ucd-lib/theme-elements/brand/ucd-theme-header/ucd-theme-header.js';
+import '@ucd-lib/theme-elements/brand/ucd-theme-primary-nav/ucd-theme-primary-nav.js';
+//import '@ucd-lib/theme-elements/brand/ucd-theme-search-popup/ucd-theme-search-popup.js';
+import '@ucd-lib/theme-elements/brand/ucd-theme-quick-links/ucd-theme-quick-links.js';
+import '@ucd-lib/theme-elements/ucdlib/ucdlib-branding-bar/ucdlib-branding-bar.js';
+// import '@ucd-lib/theme-elements/brand/ucd-theme-subnav/ucd-theme-subnav.js';
+// import '@ucd-lib/theme-elements/ucdlib/ucdlib-author-profile/ucdlib-author-profile.js';
+
+// UCD Theme Custom CSS
+
+import '@ucd-lib/theme-sass/normalize.css.js';
+import '@ucd-lib/theme-sass/1_base_html/_index.css.js';
+
+// Custom Libguides Custom Elements
+
+// Other Custom JS
